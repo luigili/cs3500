223ceeda189af74b4f6febf0c4207717c4278da9
李林溪 <luigili@yeah.net>
2016-08-31 13:27:56 -0600

    Revert "new line"
    This reverts commit 2b84f0f0cb8c1bc4861d79e5db4d8c8df6e0db95.

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -11,7 +11,6 @@ namespace Lab2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.WriteLine("c# superior, python inferior");
             Console.ReadLine();
         }
     }
